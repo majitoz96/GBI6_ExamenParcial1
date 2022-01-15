b002db1ea65555f71ed5161702e0cf805a35c900
majitoz96 <maria.auquilla@est.ikiam.edu.ec>
2022-01-14 22:54:42 -0500

    Create ejercicio_1101.sh

---
+++ directorio/ejercicio_1101.sh
@@ -0,0 +1,22 @@
+
+#1
+cd ~/CSB/unix/sandbox 
+
+#2
+du -h ../data/Marra2014_data.fasta
+
+#3
+cp ../data/Marra2014_data.fasta my_file.fasta
+
+#4
+grep -c isogroup00036 my_file.fasta
+
+#5
+cat my_file.fasta | tr -s ' ' ',' > my_file.tmp
+mv my_file.tmp my_file.fasta
+
+#6
+grep '>' my_file.fasta | cut -d ',' -f 4 | sort | uniq | wc -l
+
+#7
+grep '>' my_file.fasta | cut -d ',' -f 1,3 | sort -t '=' -k 2 -n -r | head -n 1
